e66a359a0ac32795c070588937a4219f2f8d07b1
zhoukaiping <zkp@hemashequ.com>
2019-07-03 16:55:04 +0800

    打包部署

--- src/main/java/com/zkp/shop/ShopApplication.java
+++ src/main/java/com/zkp/shop/ShopApplication.java
@@ -2,6 +2,8 @@ package com.zkp.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -9,7 +11,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication
 @EnableJpaAuditing
-public class ShopApplication {
+public class ShopApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ShopApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
